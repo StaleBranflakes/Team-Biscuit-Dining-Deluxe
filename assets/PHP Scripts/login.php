33769b35454afbc327d0ae70a820792b3fcd502d
Brandon E. <bedison8@outlook.com>
2025-11-05 13:40:06 -0500

    php updates

---
+++ assets/PHP Scripts/login.php
@@ -0,0 +1,5 @@
+<?php
+error_reporting(E_all);
+ini_set('dispplay_errors', 1);
+    
+?>
--- assets/PHP Scripts/submit-login.php
+++ assets/PHP Scripts/submit-login.php
@@ -9,22 +9,28 @@
         header("Location: Login.html?msg=" . urlencode("Missing username or password"));
         exit;
     }
-
-    $host    = "localhost";
-    $dbname  = "BiscuitDB";
-    $usr     = "root";
-    $pw      = "";
+function openDatabase(&$conID, &$dbname){
+    $host = "localhost";
+    $dbname = "BiscuitDB";
+    $usr = "root";
+    $pw = "";
 
     $conID = new mysqli($host, $usr, $pw, $dbname);
 
     if ($conID->connect_error) {
-        die("Connection Failed: " . $conID->connect_error);
+        die("Connection failed: " . $conID->connect_error);
     }
+}
+    //main execution of functions
+    openDatabase($conID, $dbname);
+
 
     $uName1 = $conID->real_escape_string($uName1);
     $pw1    = $conID->real_escape_string($pw1);
 
     $sql = "SELECT * FROM users WHERE uName = '$uName1' AND password = 'XXXX'";
     $result = $conID->query($sql);
-     
+    $conID->close();
+    
+    
 ?>
